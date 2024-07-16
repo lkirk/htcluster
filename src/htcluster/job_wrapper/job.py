01011e3e7a7ebf3b0e803b53cad84439aef9fc70
lkirk <kirklloyd@gmail.com>
2024-07-16 00:48:07 -0500

    actually, we don't want to do that

--- src/htcluster/job_wrapper/job.py
+++ src/htcluster/job_wrapper/job.py
@@ -1,7 +1,6 @@
 from functools import wraps
 
 from htcluster.logging import log_config
-from htcluster.validator_base import BaseModel
 
 
 def job_wrapper(schema):
@@ -22,17 +21,8 @@ def job_wrapper(schema):
             log_config()
             # load the validated JobArgs with (presumably) narrower types specified
             # in the job workflow entrypoint
-            match job_params:
-                case BaseModel():
-                    valid = schema.model_validate(job_params, from_attributes=True)
-                case dict():
-                    valid = schema(**job_params)
-                case _:
-                    raise ValueError(
-                        f"job_params has unexpected type: {type(job_params)}, "
-                        "expected an instance of BaseModel or dict"
-                    )
-            return func(valid)
+            valid_job_params = schema.model_validate(job_params, from_attributes=True)
+            return func(valid_job_params)
 
         return wrapper
 
