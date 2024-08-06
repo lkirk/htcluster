20c77a09d000fcd8ca3be9bfe03c2e3ee151acf0
lkirk <kirklloyd@gmail.com>
2024-08-06 13:05:19 -0500

    force local timestamps

--- src/htcluster/logging.py
+++ src/htcluster/logging.py
@@ -5,3 +5,22 @@ import structlog
 
 def log_config():
     structlog.configure(logger_factory=structlog.PrintLoggerFactory(sys.stderr))
+    processors = structlog.get_config()["processors"]
+
+    # replace default timestamper
+    ts_idx = [
+        i
+        for i, v in enumerate(processors)
+        if isinstance(v, structlog.processors.TimeStamper)
+    ]
+    if len(ts_idx) == 1:
+        ts_idx = ts_idx[0]
+        processors[ts_idx] = structlog.processors.TimeStamper(
+            # ensure that we log in local time
+            fmt="%Y-%m-%d %H:%M:%S",
+            utc=False,
+        )
+    elif len(ts_idx) > 1:
+        raise Exception(f"found more than one stamper {ts_idx}")
+
+    structlog.configure(processors=processors)
