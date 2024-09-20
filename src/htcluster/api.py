b99c654c7c5709dfb7151779c726e90572dd19a2
lkirk <kirklloyd@gmail.com>
2024-09-20 17:39:57 -0500

    split API to avoid dependency issues

--- src/htcluster/api.py
+++
@@ -1,4 +0,0 @@
-from .job_submit.__main__ import run_cluster_job
-from .job_wrapper.job import job_wrapper
-from .logging import log_config
-from .validator_base import BaseModel
---
+++ src/htcluster/api/job.py
@@ -0,0 +1,3 @@
+from ..job_wrapper.job import job_wrapper
+from ..logging import log_config
+from ..validator_base import BaseModel
